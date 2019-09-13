8b823cddc23420a8d280ef7a5b753496fea963fe
Drew <johann.miller@protonmail.com>
2019-09-12 21:16:17 -0400

    Added components for unlocked tasks

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,71 @@
 <script>
-	export let name;
+	import Unlock from './components/unlock.svelte'
+
+	let unlocks = [
+		{name: 'Aviansies', unlocked: false},
+		{name: 'Bosses', unlocked: false},
+		{name: 'Lizardmen', unlocked: false},
+		{name: 'Mithril dragons', unlocked: false},
+		{name: 'Red dragons', unlocked: false},
+		{name: 'TzHaar', unlocked: false}
+	]
+
+	let unlockGroup = []
 </script>
 
 <style>
-	h1 {
-		color: purple;
+	main {
+		display: flex;
+		flex-flow: column;
+		align-items: center;
+		padding: 3rem 1rem;
 	}
+
+	header {
+		display: flex;
+		flex-flow: column;
+		align-items: center;
+		max-width: 35rem;
+		padding: 2rem 1rem;
+
+		border-bottom: 1px #e7e7e7 solid;
+	}
+
+	header h1 {
+		margin-bottom: 2rem;
+	}
+
+	header p {
+		text-align: center;
+	}
+
+	section {
+		margin-top: 2rem;
+	}
+
+	
 </style>
 
-<h1>Hello {name}!</h1>
+<main>
+	<header>
+		<h1>Slayer calculator</h1>
+		<p>
+			A simple app that calculates the chances of each slayer assignment
+		</p>
+	</header>
+	<section>
+		<form action="" onsubmit="event.preventDefault()">
+			<label for="slayer-level">Slayer level</label>
+			<input type="number" name="slayer-level" min="1" max="99">
+			<label>Unlocked tasks</label>
+			<ul>
+				{#each unlocks as unlock, i}
+					<li>
+						<Unlock unlock={unlock}/>
+					</li>
+				{/each}
+			</ul>
+			<label>Blocked tasks</label>
+		</form>
+	</section>
+</main>
---
+++ src/components/unlock.svelte
@@ -0,0 +1,45 @@
+<script>
+  export let unlock
+
+  function toggleUnlock() {
+    unlock.unlocked = !unlock.unlocked
+    console.log(unlock.unlocked)
+  }
+
+  // TODO store checked unlock in a store
+</script>
+
+<style>
+  .checkbox-button {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		position: relative;
+		width: 100%;
+		padding: 0.5rem;
+		margin: 0.5rem 0;
+
+		background: #fafafa;
+		border: 2px #e7e7e7 solid;
+		border-radius: 0.3rem;
+		color: inherit;
+		cursor: pointer;
+	}
+
+	.checkbox-button input[type="checkbox"] {
+		opacity: 0;
+		width: 0;
+		height: 0;
+		margin: 0;
+		position: absolute;
+	}
+
+	.unlocked {
+		background: #f9e9cc;
+		border: 2px #e59400 solid;
+	}
+</style>
+
+<label class="checkbox-button" class:unlocked={unlock.unlocked} on:click|preventDefault={toggleUnlock}>
+	<input type="checkbox" value="{unlock.name}">{unlock.name}
+</label>
